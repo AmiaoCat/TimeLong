f85e91e60e7c7c95f2d3943ced7284f553aa9bd0
Ss <ss@Ss.local>
2015-05-26 16:31:56 +0800

    Initial Commit

---
+++ TimeLong/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TimeLong
+//
+//  Created by Ss on 15/5/26.
+//  Copyright (c) 2015年 Kevin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
